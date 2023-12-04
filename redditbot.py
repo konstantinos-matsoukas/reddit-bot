d65ddba02e7bc227d1f542441186709e81a18798
konstantinos-matsoukas <kostasmats9@gmail.com>
2023-12-04 11:07:16 +0200

    comment bot

--- redditbot.py
+++ redditbot.py
@@ -1,8 +1,24 @@
 import praw
 import config
+import time
 
-praw.Reddit(username = config.username, 
-            password = config.password, 
-            client_id = config.client_id, 
-            client_secret = config.client_secret, 
-            user_agent = "Nuggets test app v0.1")
+def bot_login():
+    praw.Reddit(username = config.username, 
+                password = config.password, 
+                client_id = config.client_id, 
+                client_secret = config.client_secret, 
+                user_agent = "Nuggets test app v0.1")
+    return r
+
+def run_bot(r):
+    for comment in r.subreddit('test').comments(limit=25):
+        if "dog" in comment.body:
+            print ("test")
+            comment.reply("test response")
+            print ("responded")
+
+    time.sleep(10)
+
+
+r = bot_login()
+run_bot(r)
